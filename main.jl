2857506edd9283b8c5fb2bc98ce363325434025e
Louis Latournerie <lmm.latournerie@gmail.com>
2025-07-24 15:12:04 +0200

    fix threads

--- main.jl
+++ main.jl
@@ -1,7 +1,9 @@
 include("src/simulation/return.jl")
 
-using Threads
+using Base.Threads
 using Plots
+
+println(Threads.nthreads())
 
 T = 2.
 C = [1 .5 .3; .5 1 .4; .3 .4 1]
@@ -18,12 +20,12 @@ model = DefiMarkovitzModel(
 )
 
 function full_simulation()
-    M1 = Vector(undef, 100)
-    M2 = Vector(undef, 100)
+    M1 = Vector(undef, 200)
+    M2 = Vector(undef, 200)
     m1_lock = ReentrantLock()
     m2_lock = ReentrantLock()
 
-    Threads.@threads for i in 1:100
+    Threads.@threads for i in 1:200
         D = get_returns_distribution(model, p0, 2_000)
         @lock m1_lock M1[i] = mean(D)
         @lock m2_lock M2[i] = mean(D.^2)
@@ -35,7 +37,7 @@ M1, M2 = full_simulation()
 
 plot1 = histogram(M1; fmt = :png)
 plot!([0.8503], seriestype = :vline)
-plot2 = histogram(M1; fmt = :png)
+plot2 = histogram(M2; fmt = :png)
 plot!([2.4197], seriestype = :vline)
 
 savefig(plot1, "plot1.png")
